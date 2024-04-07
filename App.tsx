50e6b4fef311a0ccafb87e5f64852a2366ff69aa
Suleman Soren <chintutudu2@gmail.com>
2024-04-07 23:13:23 +0530

    Revert "chore: google login implemented"
    This reverts commit 54f30246fc28039f5be26d56f80ac3b4056d105b.

--- App.tsx
+++ App.tsx
@@ -1,25 +1,14 @@
-import {SafeAreaView, Button, FlatList, Text} from 'react-native';
-import React, {useState} from 'react';
-import NativeAllAppModule from './src/NativeModules/NativeModule';
+import {SafeAreaView, NativeModules, Button} from 'react-native';
+import React from 'react';
+
+const {AllAppModule} = NativeModules;
 
 const App = () => {
-  const [apps, setApps] = useState<string[]>([]);
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView>
       <Button
         title="Show Alert"
-        onPress={() =>
-          NativeAllAppModule.getInstalledApps(callback => {
-            console.log(callback);
-            setApps(callback);
-          })
-        }
-      />
-      <FlatList
-        data={apps}
-        renderItem={({item}) => (
-          <Text style={{marginVertical: 10}}>{item}</Text>
-        )}
+        onPress={() => AllAppModule.getAllApp('testName', 'testLocation')}
       />
     </SafeAreaView>
   );
